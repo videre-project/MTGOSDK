329a505f192c482cef4dd7d51b55c209823f3a5b
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-08-25 14:22:50 -0500

    Document MTGOTypes usage

--- src/MTGOInjector/MTGOTypes.cs
+++ src/MTGOInjector/MTGOTypes.cs
@@ -8,6 +8,9 @@ namespace MTGOInjector;
 
 public static class MTGOTypes
 {
+  /// <summary>
+  /// Named tuple of class, base, and interface names.
+  /// </summary>
   public static Tuple<string, string?, string?> TypeProps(
     string Class,
     string? Base=null,
@@ -16,42 +19,48 @@ public static class MTGOTypes
     return new (Class, Base, Interface);
   }
 
+  /// <summary>
+  /// Map of MTGO class names to their respective assembly paths and properties.
+  /// </summary>
   public static Dictionary<string, Tuple<string, string?, string?>> Map =>
     new()
     {
       // Core
       {"DialogService",
-        TypeProps( Class: "Shiny.Core.DialogManagement.DialogService",
+        TypeProps( Class:     "Shiny.Core.DialogManagement.DialogService",
                    Interface: "Shiny.Core.Interfaces.IDialogService" )},
       {"GenericDialogViewModel",
-        TypeProps( Class: "Shiny.ViewModels.GenericDialogViewModel",
-                   Base: "Shiny.ViewModels.BasicDialogViewModelBase" )},
+        TypeProps( Class:     "Shiny.ViewModels.GenericDialogViewModel",
+                   Base:      "Shiny.ViewModels.BasicDialogViewModelBase" )},
       // FlsClient
       {"FlsClientSession",
-        TypeProps( Class: "FlsClient.FlsClientSession",
-                   Base: "FlsClient.ClientSessionBase",
+        TypeProps( Class:     "FlsClient.FlsClientSession",
+                   Base:      "FlsClient.ClientSessionBase",
                    Interface: "FlsClient.Interface.IFlsClientSession" )},
       // MTGO -> Shiny
       {"App",
-        TypeProps( Class: "Shiny.App",
-                   Base: "System.Windows.Application" )},
+        TypeProps( Class:     "Shiny.App",
+                   Base:      "System.Windows.Application" )},
       // PlayScene -> Shiny.Play
       {"PlaySceneViewModel",
-        TypeProps( Class: "Shiny.Play.PlaySceneViewModel",
-                   Base: "Shiny.Core.SceneViewModel",
+        TypeProps( Class:     "Shiny.Play.PlaySceneViewModel",
+                   Base:      "Shiny.Core.SceneViewModel",
                    Interface: "Shiny.Core.Interfaces.IPlaySceneViewModel" )},
       // WotC.MtGO.Client.Common
       {"ObjectProvider",
-        TypeProps( Class: "WotC.MtGO.Client.Common.ServiceLocation.ObjectProvider" )},
+        TypeProps( Class:     "WotC.MtGO.Client.Common.ServiceLocation.ObjectProvider" )},
       // WotC.MtGO.Client.Model.Core
       {"Utility",
-        TypeProps( Class: "WotC.MtGO.Client.Model.Core.Utility" )},
+        TypeProps( Class:     "WotC.MtGO.Client.Model.Core.Utility" )},
       // WotC.MtGO.Client.Model.Play
       {"Match",
-        TypeProps( Class: "WotC.MtGO.Client.Model.Play.MatchEvent.Match",
-                   Base: "WotC.MtGO.Client.Model.Play.MatchEvent.MatchBase" )},
+        TypeProps( Class:     "WotC.MtGO.Client.Model.Play.MatchEvent.Match",
+                   Base:      "WotC.MtGO.Client.Model.Play.MatchEvent.MatchBase" )},
     };
 
+  /// <summary>
+  /// Gets the MTGO assembly path for a given class for a given property type.
+  /// </summary>
   public static string Get(string name, string key = "Class") =>
 #pragma warning disable CS8603
     key switch
