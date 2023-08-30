9475012db502dd6837a0e171afb7fc1e8adc8a57
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-08-30 06:54:36 -0500

    [third_party/RemoteNET] Simplify DEBUG logging conditions

--- third_party/RemoteNET/src/RemoteNET/Logger.cs
+++ third_party/RemoteNET/src/RemoteNET/Logger.cs
@@ -1,28 +1,32 @@
 ﻿using System;
 using System.Diagnostics;
 
+
 namespace RemoteNET
 {
-    internal class Logger
+  internal class Logger
+  {
+    public static Lazy<bool> DebugInRelease = new Lazy<bool>(() =>
+      !string.IsNullOrWhiteSpace(
+          Environment.GetEnvironmentVariable("REMOTE_NET_DIVER_MAGIC_DEBUG")));
+
+  #if DEBUG
+    public static bool IsDebug = true;
+  #else
+    public static bool IsDebug = false;
+  #endif
+
+    internal static void Debug(string s)
     {
-        internal static Lazy<bool> _debugInRelease = new Lazy<bool>(() => !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("REMOTE_NET_CLIENT_MAGIC_DEBUG")));
-
-        internal static void Debug(string s)
-        {
-#if DEBUG
-#else // RELEASE
-            // We want to allow debug writing in release only if the magic enviroment var is set.
-            // in debug, this `if` does not exist and logging to console always takes place.
-            if(_debugInRelease.Value)
-#endif
-            {
-                Console.WriteLine(s);
-            }
-
-            if(Debugger.IsAttached)
-            {
-                System.Diagnostics.Debug.WriteLine(s);
-            }
-        }
+      if (IsDebug || Debugger.IsAttached)
+      {
+        System.Diagnostics.Debug.WriteLine(s);
+      }
+      // Allow debug logging in release only if the environment variable is set.
+      else if(DebugInRelease.Value)
+      {
+        Console.WriteLine(s);
+      }
     }
+  }
 }
--- third_party/RemoteNET/src/ScubaDiver/Logger.cs
+++ third_party/RemoteNET/src/ScubaDiver/Logger.cs
@@ -10,21 +10,22 @@ internal class Logger
     !string.IsNullOrWhiteSpace(
         Environment.GetEnvironmentVariable("REMOTE_NET_DIVER_MAGIC_DEBUG")));
 
+#if DEBUG
+  public static bool IsDebug = true;
+#else
+  public static bool IsDebug = false;
+#endif
+
   internal static void Debug(string s)
   {
-#if DEBUG
-#else // RELEASE
-    // We want to allow debug writing in release only if the magic environment var is set.
-    // in debug, this `if` does not exist and logging to console always takes place.
-    if(DebugInRelease.Value)
-#endif
-    {
-      Console.WriteLine(s);
-    }
-
-    if (Debugger.IsAttached)
+    if (IsDebug || Debugger.IsAttached)
     {
       System.Diagnostics.Debug.WriteLine(s);
+    }
+    // Allow debug logging in release only if the environment variable is set.
+    else if(DebugInRelease.Value)
+    {
+      Console.WriteLine(s);
     }
   }
 }
