956a7ee3cdca88692e478cbd23aeca629f38ac19
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-10-16 00:15:33 -0500

    [MTGOSDK/API] Move UserManager wrapper methods to new class

--- MTGOSDK/src/API/User.cs
+++ MTGOSDK/src/API/User.cs
@@ -16,91 +16,26 @@ namespace MTGOSDK.API;
 
 public class User(dynamic /* IUser */ user)
 {
-  //
-  // UserManager wrapper methods
-  //
-
   /// <summary>
-  /// This class manages the client's caching and updating of user information.
+  /// Internal unwrapped reference to any captured IUser objects.
   /// </summary>
-  private static readonly dynamic s_userManager =
-    //
-    // We must call the internal GetInstance() method to retrieve PropertyInfo
-    // data from the remote type as the local proxy type or ObjectProvider will
-    // restrict access to internal or private members.
-    //
-    // This is a limitation of the current implementation of the Proxy<T> type
-    // since any MemberInfo data is cached by the runtime and will conflict
-    // with RemoteNET's internal type reflection methods.
-    //
-    RemoteClient.GetInstance("WotC.MtGO.Client.Model.Core.UserManager");
+  /// <remarks>
+  /// This is used to extract the (dynamic) IUser object from any passed in
+  /// User objects, deferring any dynamic dispatching of User constructors.
+  /// </remarks>
+  private dynamic user = user is User ? user.user : user
+    ?? throw new Exception($"User object is not a valid IUser type.");
 
-  /// <summary>
-  /// Retrieves a user object from the client's UserManager.
-  /// </summary>
-  /// <param name="id">The Login ID of the user.</param>
-  /// <param name="name">The display name of the user.</param>
-  /// <returns>A new User object.</returns>
-  private static User GetUser(int id, string name) =>
-    new User(
-      s_userManager.CreateNewUser(id, name)
-        ?? throw new Exception($"Failed to retrieve user '{name}' (#{id}).")
-    );
-
-  /// <summary>
-  /// Retrieves a user object from the client's UserManager.
-  /// </summary>
-  /// <param name="name">The display name of the user.</param>
-  /// <returns>A new User object.</returns>
-  private static User GetUser(string name) =>
-    GetUser(
-      GetUserId(name)
-        ?? throw new Exception($"User '{name}' does not exist."),
-      name
-    );
-
-  /// <summary>
-  /// Retrieves a user object from the client's UserManager.
-  /// </summary>
-  /// <param name="id">The Login ID of the user.</param>
-  /// <returns>A new User object.</returns>
-  private static User GetUser(int id) =>
-    GetUser(
-      id,
-      GetUserName(id)
-        ?? throw new Exception($"User #{id} does not exist.")
-    );
-
-  /// <summary>
-  /// Retrieves the username of a user by their Login ID.
-  /// </summary>
-  /// <param name="id">The Login ID of the user.</param>
-  /// <returns>The display name of the user.</returns>
-  public static string GetUserName(int id) => s_userManager.GetUserName(id);
-
-  /// <summary>
-  /// Retrieves the Login ID of a user by their username.
-  /// </summary>
-  /// <param name="name">The display name of the user.</param>
-  /// <returns>The Login ID of the user.</returns>
-  public static int? GetUserId(string name) => s_userManager.GetUserId(name);
-
-  public User(int id) : this(GetUser(id))
+  public User(int id) : this(UserManager.GetUser(id))
   { }
-  public User(string name) : this(GetUser(name))
+  public User(string name) : this(UserManager.GetUser(name))
   { }
-  public User(int id, string name) : this(GetUser(id, name))
+  public User(int id, string name) : this(UserManager.GetUser(id, name))
   { }
 
   //
   // IUser wrapper properties
   //
-
-  /// <summary>
-  /// Internal unwrapped reference to any captured IUser objects.
-  /// </summary>
-  private dynamic user = user is User ? user.user : user
-    ?? throw new Exception($"User object is not a valid IUser type.");
 
   /// <summary>
   /// The Login ID of the user.
---
+++ MTGOSDK/src/API/UserManager.cs
@@ -0,0 +1,87 @@
+/** @file
+  Copyright (c) 2023, Cory Bennett. All rights reserved.
+  SPDX-License-Identifier: Apache-2.0
+**/
+
+using System.Reflection;
+
+using MTGOSDK.Core;
+using MTGOSDK.Core.Reflection;
+
+using WotC.MtGO.Client.Model;
+using WotC.MtGO.Client.Model.Core;
+
+
+namespace MTGOSDK.API;
+
+public static class UserManager
+{
+  //
+  // UserManager wrapper methods
+  //
+
+  /// <summary>
+  /// This class manages the client's caching and updating of user information.
+  /// </summary>
+  private static readonly dynamic s_userManager =
+    //
+    // We must call the internal GetInstance() method to retrieve PropertyInfo
+    // data from the remote type as the local proxy type or ObjectProvider will
+    // restrict access to internal or private members.
+    //
+    // This is a limitation of the current implementation of the Proxy<T> type
+    // since any MemberInfo data is cached by the runtime and will conflict
+    // with RemoteNET's internal type reflection methods.
+    //
+    RemoteClient.GetInstance("WotC.MtGO.Client.Model.Core.UserManager");
+
+  /// <summary>
+  /// Retrieves a user object from the client's UserManager.
+  /// </summary>
+  /// <param name="id">The Login ID of the user.</param>
+  /// <param name="name">The display name of the user.</param>
+  /// <returns>A new User object.</returns>
+  public static User GetUser(int id, string name) =>
+    new User(
+      s_userManager.CreateNewUser(id, name)
+        ?? throw new Exception($"Failed to retrieve user '{name}' (#{id}).")
+    );
+
+  /// <summary>
+  /// Retrieves a user object from the client's UserManager.
+  /// </summary>
+  /// <param name="name">The display name of the user.</param>
+  /// <returns>A new User object.</returns>
+  public static User GetUser(string name) =>
+    GetUser(
+      GetUserId(name)
+        ?? throw new Exception($"User '{name}' does not exist."),
+      name
+    );
+
+  /// <summary>
+  /// Retrieves a user object from the client's UserManager.
+  /// </summary>
+  /// <param name="id">The Login ID of the user.</param>
+  /// <returns>A new User object.</returns>
+  public static User GetUser(int id) =>
+    GetUser(
+      id,
+      GetUserName(id)
+        ?? throw new Exception($"User #{id} does not exist.")
+    );
+
+  /// <summary>
+  /// Retrieves the username of a user by their Login ID.
+  /// </summary>
+  /// <param name="id">The Login ID of the user.</param>
+  /// <returns>The display name of the user.</returns>
+  public static string GetUserName(int id) => s_userManager.GetUserName(id);
+
+  /// <summary>
+  /// Retrieves the Login ID of a user by their username.
+  /// </summary>
+  /// <param name="name">The display name of the user.</param>
+  /// <returns>The Login ID of the user.</returns>
+  public static int? GetUserId(string name) => s_userManager.GetUserId(name);
+}
