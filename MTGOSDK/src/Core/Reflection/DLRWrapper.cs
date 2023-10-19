19648ae61acb89dcc0e392392691e89e9f3ab7be
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-10-19 02:15:38 -0500

    [MTGOSDK/Core] Reflection: Fix comments for DLRWrapper

--- MTGOSDK/src/Core/Reflection/DLRWrapper.cs
+++ MTGOSDK/src/Core/Reflection/DLRWrapper.cs
@@ -6,13 +6,12 @@
 using System.Reflection;
 
 using MTGOSDK.Core;
-using MTGOSDK.Core.Reflection;
 
 
 namespace MTGOSDK.Core.Reflection;
 
 /// <summary>
-/// A wrapper for dynamic objects that implements an interface at runtime.
+/// A wrapper for dynamic objects that implement an interface at runtime.
 /// </summary>
 /// <remarks>
 /// This class exposes an overrideable <see cref="obj"/> property that is used
@@ -21,13 +20,16 @@ namespace MTGOSDK.Core.Reflection;
 /// the base class constructor has completed, exposing the captured dynamic
 /// object to derived classes with the <see cref="@base"/> property.
 /// </remarks>
-/// <typeparam name="T"></typeparam>
+/// <typeparam name="T">The interface type to wrap.</typeparam>
 public class DLRWrapper<T>() where T : class
 {
   /// <summary>
   /// This is the internal reference for any dynamic or derived class objects.
   /// </summary>
   internal virtual dynamic obj =>
+    //
+    // Derived classes must override this property to capture dynamic objects.
+    //
     throw new Exception(
         $"{nameof(DLRWrapper<T>)}.obj must capture a {typeof(T).Name} type.");
 
