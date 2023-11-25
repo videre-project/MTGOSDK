912b5e6c08a6da02822611b42998a0d79700afc6
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-11-25 01:17:41 -0600

    [MTGOSDK/API] Events: Add generic EventCallback delegate type
    Allows for another way of subscribing to EventProxy handlers using only their EventArgs type.

---
+++ MTGOSDK/src/API/Events/BaseEvent.cs
@@ -0,0 +1,27 @@
+/** @file
+  Copyright (c) 2023, Cory Bennett. All rights reserved.
+  SPDX-License-Identifier: Apache-2.0
+**/
+
+
+namespace MTGOSDK.API;
+
+/// <summary>
+/// EventHandler wrapper types used by the API.
+/// </summary>
+/// <remarks>
+/// This class contains wrapper types for events importable via
+/// <br/>
+/// <c>using static MTGOSDK.API.Events;</c>.
+/// </remarks>
+public sealed partial class Events
+{
+  //
+  // EventHandler delegate types
+  //
+
+  /// <summary>
+  /// Delegate type for subscribing to a generic event type.
+  /// </summary>
+  public delegate void EventCallback<T>(T args) where T : class;
+}
