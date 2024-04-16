9a56be2411a470b25acc31aa60dd92b5bb614920
Cory Bennett <csquaredbennett@gmail.com>
2024-04-15 19:39:21 -0500

    Update SDK examples
    Updates comments and usage for clarity

--- examples/BasicBot/Program.cs
+++ examples/BasicBot/Program.cs
@@ -1,23 +1,34 @@
 /** @file
-  Copyright (c) 2023, Cory Bennett. All rights reserved.
+  Copyright (c) 2024, Cory Bennett. All rights reserved.
   SPDX-License-Identifier: Apache-2.0
 **/
 
 using System;
+using System.Threading.Tasks;
 
 using MTGOSDK.API;
 using MTGOSDK.Core.Remoting;
 using MTGOSDK.Core.Security;
 
 
+// Wait until the main MTGO server is online.
+bool restart = false;
+while (!await ServerStatus.IsOnline())
+{
+  Console.WriteLine("MTGO servers are currently offline. Waiting...");
+  await Task.Delay(TimeSpan.FromMinutes(30));
+  restart |= true; // Restart after downtime.
+}
+
+// Initialize the client instance.
 Console.WriteLine($"Connecting to MTGO v{Client.CompatibleVersion}...");
 using var client = new Client(
-  RemoteClient.HasStarted
+  !restart && RemoteClient.HasStarted
     ? new ClientOptions()
     : new ClientOptions
       {
         CreateProcess = true,
-        // DestroyOnExit = true,
+        DestroyOnExit = true,
         AcceptEULAPrompt = true
       }
 );
@@ -25,10 +36,20 @@ using var client = new Client(
 if (!Client.IsConnected)
 {
   DotEnv.LoadFile();
+  // Waits until the client has loaded and is ready.
   await client.LogOn(
-    username: DotEnv.Get("USERNAME"),
-    password: DotEnv.Get("PASSWORD")
+    username: DotEnv.Get("USERNAME"), // String value
+    password: DotEnv.Get("PASSWORD")  // SecureString value
   );
+  Console.WriteLine($"Connected as {Client.CurrentUser.Name}.");
 }
 
-Console.WriteLine($"Finished loading.");
+// Teardown the bot when the MTGO client disconnects.
+client.IsConnectedChanged += delegate(object? sender)
+{
+  Console.WriteLine("The MTGO client has been disconnected. Stopping...");
+  client.Dispose();
+  Environment.Exit(-1);
+};
+
+Console.WriteLine("Finished loading.");
--- examples/GameTracker/Program.cs
+++ examples/GameTracker/Program.cs
@@ -1,19 +1,31 @@
 /** @file
-  Copyright (c) 2023, Cory Bennett. All rights reserved.
+  Copyright (c) 2024, Cory Bennett. All rights reserved.
   SPDX-License-Identifier: Apache-2.0
 **/
 
 using System;
 
-using MTGOSDK.API;
 using MTGOSDK.API.Play.History;
 
 
-var client = new Client();
-
+//
+// We implicitly instantiate the RemoteClient through accessing HistoryManager.
+//
+// Because we do not explicitly create a MTGOSDK.API.Client instance, we are
+// expecting to connect to an already running instance of the MTGO client
+// without any special permissions or configuration.
+//
 if (!HistoryManager.HistoryLoaded)
-  throw new Exception("Game history not loaded.");
+  throw new OperationCanceledException("History not loaded.");
 
+//
+// Here we iterate through items collected by the MTGO client's HistoryManager,
+// which include all games, matches, and tournaments played by the current user.
+//
+// This is broken down roughly by event type, which is wrapped into the two
+// HistoricalMatch and HistoricalTournament classes; the HistoricalItem class
+// is the base class for all items in the history.
+//
 foreach(var item in HistoryManager.Items)
 {
   var type = item.GetType().Name;
@@ -22,11 +34,13 @@ foreach(var item in HistoryManager.Items)
   {
     case "HistoricalItem":
     case "HistoricalMatch":
-      Console.WriteLine($"  Id: {item.Id}");
+      Console.WriteLine($"  Match Id: {item.Id}");
+      // For brevity, we'll select only the first game id.
       Console.WriteLine($"  Game 1 Id: {item.GameIds[0]}");
       break;
     case "HistoricalTournament":
-      Console.WriteLine($"  Id: {item.Id}");
+      Console.WriteLine($"  Tournament Id: {item.Id}");
+      // For brevity, we'll select only the first match id.
       Console.WriteLine($"  Match 1 Id: {item.Matches.First().Id}");
       break;
   }
