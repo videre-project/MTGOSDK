b767114f615d967794cb0ce250f8a1a7e86118c2
Cory Bennett <csquaredbennett@gmail.com>
2024-05-04 19:13:48 -0500

    [third_party/ScubaDiver] Provide console in debug mode

--- third_party/ScubaDiver/src/DllEntry.cs
+++ third_party/ScubaDiver/src/DllEntry.cs
@@ -34,11 +34,16 @@ public class DllEntry
 
   public static int EntryPoint(string pwzArgument)
   {
+    // If we need to log and a debugger isn't attached to the target process
+    // then we need to allocate a console and redirect STDOUT to it.
+    Logger.RedirectConsole();
+
     //
     // The Bootstrapper needs to call a C# function with exactly this signature,
     // so we use it to just create a diver, and run the Start func (blocking)
     //
     ParameterizedThreadStart func = DiverHost;
+    Logger.Debug($"[EntryPoint] Starting ScubaDiver with argument: {pwzArgument}");
     Thread diverHostThread = new(func);
     diverHostThread.Start(pwzArgument);
 
--- third_party/ScubaDiver/src/Logger.cs
+++ third_party/ScubaDiver/src/Logger.cs
@@ -6,17 +6,55 @@
 
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32.SafeHandles;
 
 
 namespace ScubaDiver;
 
 internal class Logger
 {
+  #region P/Invoke Console Spawning
+
+  [DllImport("kernel32.dll",
+    EntryPoint = "GetStdHandle",
+    SetLastError = true,
+    CharSet = CharSet.Auto,
+    CallingConvention = CallingConvention.StdCall)]
+  private static extern IntPtr GetStdHandle(int nStdHandle);
+
+  [DllImport("kernel32.dll",
+    EntryPoint = "AllocConsole",
+    SetLastError = true,
+    CharSet = CharSet.Auto,
+    CallingConvention = CallingConvention.StdCall)]
+  [return: MarshalAs(UnmanagedType.Bool)]
+  private static extern bool AllocConsole();
+
+  private const int STD_OUTPUT_HANDLE = -11;
+
+  #endregion
+
 #if DEBUG
   public static bool IsDebug = true;
 #else
   public static bool IsDebug = false;
 #endif
+
+  public static void RedirectConsole()
+  {
+    if (IsDebug && !Debugger.IsAttached && AllocConsole())
+    {
+      IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+      SafeFileHandle safeFileHandle = new(stdHandle, true);
+      FileStream fileStream = new(safeFileHandle, FileAccess.Write);
+      Encoding encoding = Encoding.ASCII;
+      StreamWriter standardOutput = new(fileStream, encoding) { AutoFlush = true };
+      Console.SetOut(standardOutput);
+    }
+  }
 
   internal static void Debug(string s)
   {
