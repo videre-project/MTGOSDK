8013c0c69f1c348f480b391617263138e0216226
Cory Bennett <csquaredbennett@gmail.com>
2024-05-28 10:13:06 -0500

    [MTGOSDK/API] ObjectProvider: Add caching and deferral mechanism, refresh method
    Adds an internal cache and deferral mechanism for allowing re-use of existing instances, as well as a Refresh() method for fetching new instances from the client and updating existing references.

--- MTGOSDK/src/API/ObjectProvider.cs
+++ MTGOSDK/src/API/ObjectProvider.cs
@@ -3,9 +3,13 @@
   SPDX-License-Identifier: Apache-2.0
 **/
 
+using System.Collections.Concurrent;
+
 using MTGOSDK.Core.Logging;
-using MTGOSDK.Core.Remoting;
 using MTGOSDK.Core.Reflection;
+using MTGOSDK.Core.Remoting;
+using MTGOSDK.Core.Remoting.Internal;
+using static MTGOSDK.Core.Reflection.DLRWrapper<dynamic>;
 
 
 namespace MTGOSDK.API;
@@ -22,28 +26,56 @@ public static class ObjectProvider
     new(typeof(WotC.MtGO.Client.Common.ServiceLocation.ObjectProvider));
 
   /// <summary>
-  /// Returns an instance of the given type from the client's ObjectProvider.
+  /// Cache of all instances retrieved from the client's ObjectProvider.
   /// </summary>
-  /// <param name="queryPath">The query path of the registered type.</param>
-  /// <returns>A remote instance of the given type.</returns>
-  public static dynamic Get(string queryPath)
-  {
-    // Get the RemoteType from the type's query path
-    Log.Trace("Retrieving instance type {Type}", queryPath);
-    Type genericType = RemoteClient.GetInstanceType(queryPath);
-
-    // Invoke the Get<T>() method on the client's ObjectProvider class
-    return RemoteClient.InvokeMethod(s_proxy,
-        methodName: "Get",
-        genericTypes: new Type[] { genericType });
-  }
+  private static readonly ConcurrentDictionary<string, dynamic> s_instances = new();
 
   /// <summary>
   /// Returns an instance of the given type from the client's ObjectProvider.
   /// </summary>
-  /// <typeparam name="T">The class or interface type to retrieve.</typeparam>
+  /// <param name="queryPath">The query path of the registered type.</param>
+  /// <param name="useCache">Whether to use the cached instance.</param>
+  /// <param name="useHeap">Whether to query the client's object heap.</param>
   /// <returns>A remote instance of the given type.</returns>
-  public static T Get<T>() where T : class => Get<T>(bindTypes: true);
+  public static dynamic Get(
+    string queryPath,
+    bool useCache = true,
+    bool useHeap = false)
+  {
+    // Check if the instance is already cached
+    if (useCache && s_instances.TryGetValue(queryPath, out dynamic instance))
+    {
+      Log.Trace("Retrieved cached instance type {Type}", queryPath);
+      return instance;
+    }
+
+    // If the client is disposed, return an empty instance to defer construction
+    if (RemoteClient.IsDisposed)
+    {
+      instance = new DynamicRemoteObject();
+    }
+    // Query using the ObjectProvider.Get<T>() method on the client
+    else if (!useHeap)
+    {
+      // Get the RemoteType from the type's query path
+      Log.Trace("Retrieving instance type {Type}", queryPath);
+      Type genericType = RemoteClient.GetInstanceType(queryPath);
+
+      // Invoke the Get<T>() method on the client's ObjectProvider class
+      instance = RemoteClient.InvokeMethod(s_proxy, "Get", [genericType]);
+    }
+    // Query the for the instance type from the client's object heap
+    else
+    {
+      Log.Trace("Retrieving heap instance of type {Type}", queryPath);
+      instance = RemoteClient.GetInstance(queryPath);
+    }
+
+    // Cache the instance for future use
+    s_instances.TryAdd(queryPath, instance);
+
+    return instance;
+  }
 
   /// <summary>
   /// Returns an instance of the given type from the client's ObjectProvider.
@@ -65,8 +97,7 @@ public static class ObjectProvider
     //
     try
     {
-      if(bindTypes == false)
-        return RemoteClient.GetInstance(proxy);
+      if(bindTypes == false) return Get(proxy, useHeap: true);
     }
     catch { /* Input type was not instantiated/instantiable on the client. */ }
 
@@ -79,5 +110,29 @@ public static class ObjectProvider
       obj = Proxy<dynamic>.As(obj, @interface ?? proxy.Class);
 
     return obj;
+  }
+
+  /// <summary>
+  /// Returns an instance of the given type from the client's ObjectProvider.
+  /// </summary>
+  /// <typeparam name="T">The class or interface type to retrieve.</typeparam>
+  /// <returns>A remote instance of the given type.</returns>
+  public static T Get<T>() where T : class => Get<T>(bindTypes: true);
+
+  /// <summary>
+  /// Refreshes all instances in the ObjectProvider cache.
+  /// </summary>
+  /// <remarks>
+  /// This method is useful for updating all instances in the cache after
+  /// connecting to a new MTGO instance or changing the client's context.
+  /// </remarks>
+  public static void Refresh()
+  {
+    foreach (var kvp in s_instances)
+    {
+      dynamic refObj = s_instances[kvp.Key];
+      dynamic obj = Get(kvp.Key, useCache: false);
+      Swap(ref refObj, obj, bindTypes: true);
+    }
   }
 }
