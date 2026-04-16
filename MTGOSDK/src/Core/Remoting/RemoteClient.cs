e6616b6ebe24a593b03b88269ad6136e3dcc0874
Cory Bennett <csquaredbennett@gmail.com>
2026-04-16 07:47:36 -0500

    Revert "[MTGOSDK/Core/Remoting] RemoteClient: Fix ClickOnce locking false-positives"
    This reverts commit 8cac5d1fab3c9df12f7c5917b772544582978a2c.

--- MTGOSDK/src/Core/Remoting/RemoteClient.cs
+++ MTGOSDK/src/Core/Remoting/RemoteClient.cs
@@ -137,8 +137,7 @@ public sealed class RemoteClient : DLRWrapper
   public static Process? MTGOProcess(bool throwOnFailure = false)
   {
     // If we already have a ClientProcess defined, return it.
-    if (!(_isDisposing || _isDisposed) &&
-        ClientProcess != null && HasLoadedClr(ClientProcess))
+    if (!(_isDisposing || _isDisposed) && ClientProcess != null)
       return ClientProcess;
 
     //
@@ -192,8 +191,6 @@ public sealed class RemoteClient : DLRWrapper
           Try<bool>(() =>
             p.Threads.Count > 0 &&
             p.StartTime != DateTime.MinValue))
-        // Ignore restart/update leftovers that have not loaded the CLR yet.
-        .Where(HasLoadedClr)
         .OrderByDescending(p => p.StartTime)
         .FirstOrDefault(),
       fallback: null);
@@ -202,19 +199,6 @@ public sealed class RemoteClient : DLRWrapper
       throw new NullReferenceException("MTGO client process not found.");
 
     return process;
-  }
-
-  private static bool HasLoadedClr(Process process)
-  {
-    try
-    {
-      return process.GetModules().Any(module =>
-        module.szModule.Equals("mscoree.dll", StringComparison.InvariantCultureIgnoreCase));
-    }
-    catch
-    {
-      return false;
-    }
   }
 
   /// <summary>
